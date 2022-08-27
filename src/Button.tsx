0ee64a251747f4defa56221ed23ef5d7e9dcfdbd
AleksandraBoch <shemaFoxy@gmail.com>
2022-08-27 23:48:37 +0300

    again

---
+++ src/Button.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type ButtonType={
+    name:string
+    callBack:()=> void
+}
+
+
+export const Button=(props:ButtonType)=>{
+
+    const OnClickHandler=()=>{
+        props.callBack()
+    }
+    return(
+    <>
+    <button onClick={OnClickHandler}>{props.name}</button>
+
+    </>
+
+
+)
+
+}
